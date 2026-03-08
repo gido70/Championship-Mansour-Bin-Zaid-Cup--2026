984c9f59a357577f30c10079b412420f716c6d9f
gido70 <63196885+gido70@users.noreply.github.com>
2026-03-09 03:41:51 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = "mansour-static-v4";
-const DATA_CACHE = "mansour-data-v4";
+const STATIC_CACHE = "mansour-static-v5";
+const DATA_CACHE = "mansour-data-v5";
 
 const STATIC_ASSETS = [
   "./",
